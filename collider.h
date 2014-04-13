16654046db59abadabd6742df39fb8a540920257
kalogiri <m.rachitgiri@gmail.com>
2014-04-14 00:39:58 +0100

    Update collider.h

--- collider.h
+++ collider.h
@@ -7,12 +7,14 @@ public:
 	void updateColliderObject();
 	bool returnCollided();
 	void collisionDetect(ofPoint);
+	void reset();
 
 	ofVec2f vel;
 	ofPoint pos;
 	
 	bool 
 		collided;
+
 	collider(void);
 	~collider(void);
 };
