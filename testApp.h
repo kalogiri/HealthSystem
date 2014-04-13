a4416ebe97150e0f794661246cc537841e20826c
kalogiri <m.rachitgiri@gmail.com>
2014-04-13 21:12:22 +0100

    Create testApp.h
    The file that runs all the different classes

---
+++ testApp.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "ofMain.h"
+#include "player.h"
+#include "collider.h"
+#include "gameStates.h"
+
+class testApp : public ofBaseApp{
+	public:
+		void setup();
+		void update();
+		void draw();
+		void colliderObjectDraw();
+		void colliderObjectUpdate();
+		void colliderCollideWithPlayer();
+		void playerMove();
+
+	//Variables
+		bool 
+			up,down,right,left,
+			gameStart,gameEnd;
+
+		//The player
+		player c;
+
+		//Timer
+		vector<collider> ti;
+		collider t;
+
+		//Game state screens
+		gameStates g;
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+};
