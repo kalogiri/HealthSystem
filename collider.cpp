4d817a944e86f5ef678947207eeaccd30e76a4bd
kalogiri <m.rachitgiri@gmail.com>
2014-04-13 21:17:52 +0100

    Create collider.cpp
    The collider used for checking the collisions with the player

---
+++ collider.cpp
@@ -0,0 +1,30 @@
+#include "collider.h"
+collider::collider(void){
+	collided = false;
+	pos.set(600,ofRandom(55,330));
+	vel.set(ofRandom(-1,-3),0);
+}
+
+
+void collider::collisionDetect(ofPoint plPos) {
+	if(plPos.distance(pos) < 25) {
+		collided = true;
+	} else {
+		collided = false;
+	}
+}
+
+bool collider::returnCollided() { return collided;}
+
+void collider::updateColliderObject() {
+	pos+=vel;
+}
+
+void collider::drawColliderObject(){
+	ofSetColor(255,0,0);
+	ofFill();
+	ofRect(pos.x, pos.y, 10,10);
+}
+
+collider::~collider(void){
+}
