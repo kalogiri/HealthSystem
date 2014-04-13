7b0b36a2c1117ed052a2edb82cc3a56232e30f57
kalogiri <m.rachitgiri@gmail.com>
2014-04-13 21:13:56 +0100

    Create player.h
    the player header file

---
+++ player.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "ofMain.h"
+
+class player
+{
+public:
+
+	player(void);
+
+	//Variable
+	ofVec2f vel,pos;
+	int deathColor;
+	int colorRed;
+	int colorBlue;
+	int colorGreen;
+	int deathCount;
+	bool dead;
+	//Functions
+	void
+		playerDisplay(),
+		updatePlayer();
+	
+	~player(void);
+};
+
